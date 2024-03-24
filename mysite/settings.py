53de8cc41d7aeb0f94d3f32cfceb3b6bd81ac5cb
dixittanisha02 <dixittanisha02@gmail.com>
2024-03-24 15:35:58 +0530

    conf adds student and teachers apps conf adds rest framework

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #rest framework
+    'rest_framework',
+
+    #local apps
     'signup',
+    'student',
+    'teacher',
 ]
 
 MIDDLEWARE = [
--- mysite/urls.py
+++ mysite/urls.py
@@ -19,4 +19,6 @@ from django.urls import path , include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('signup.urls')),
+    path('student/',include('student.urls')),
+    path('teacher/',include('teacher.urls')),
 ]
