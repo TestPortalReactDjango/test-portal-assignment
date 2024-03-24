6a72b9c14d0342e5e10cc0f6d68dfc40c4035577
dixittanisha02 <dixittanisha02@gmail.com>
2024-03-24 18:45:12 +0530

    feat creates test, QrefTable, SingleCorrectQ models

--- test/models.py
+++ test/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from questions.models import qRefTable
 
 # Create your models here.
+class test(models.Model):
+    testname=models.CharField(max_length=100,blank=False,null=False)
+    numberOfQuestion=models.IntegerField()
+    startTime=models.DateTimeField()
+    endTime=models.DateTimeField()
+    qRefTable=models.ForeignKey(qRefTable, on_delete=models.CASCADE)
+
