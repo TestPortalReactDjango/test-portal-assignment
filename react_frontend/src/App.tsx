aa320c5c8dc81a7f762e9c96dfe2b998e419f4ca
dixittanisha02 <dixittanisha02@gmail.com>
2024-03-26 00:40:26 +0530

    chor adds component files feat adds routes to component files

--- react_frontend/src/App.tsx
+++ react_frontend/src/App.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Student from "./pages/Student";
+import Teacher from "./pages/Teacher";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route path="Login" element={<Login />} />
+          <Route path="Signup" element={<Signup />} />
+          <Route path="Student" element={<Student />} />
+          <Route path="Teacher" element={<Teacher />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
