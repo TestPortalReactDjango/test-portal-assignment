44062f009da982f495972f2f8a179964b72d8c35
dixittanisha02 <dixittanisha02@gmail.com>
2024-03-26 02:45:02 +0530

    feat adds home page component

---
+++ react_frontend/src/pages/Student.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Student(){
+    return(
+        <div className='Container'>
+            <div className='Box'>
+                <h2>Teacher</h2>
+                <button>Login as Teacher</button>
+            </div>
+            <div className='Box'>
+                <h2>Student</h2>
+                <button>Login as Student</button>
+                <button>SignUp as Student</button>
+            </div>
+        </div>
+    )
+}
+
+export default Student;
