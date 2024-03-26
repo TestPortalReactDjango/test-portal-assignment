725859681899db24fabff64704ab8d8aa4e2e4c5
dixittanisha02 <dixittanisha02@gmail.com>
2024-03-26 18:01:13 +0530

    chor styles home page component

--- react_frontend/src/pages/Home.tsx
+++ react_frontend/src/pages/Home.tsx
@@ -3,20 +3,20 @@ import Button from '../components/Button';
 
 const Home: React.FC=()=>{
     return(
-        <div className="flex justify-center items-center h-screen">
+        <div className="flex justify-center items-center h-screen bg-blue-200">
         <div className="bg-gray-200 p-8 rounded-lg shadow-lg">
-          <div className="flex justify-between items-center mb-4">
+          <div className="flex justify-between mb-4">
             <div className="w-1/2 mr-4">
-              <div className="mb-2">
-                <p>Text 1</p>
+              <div className="mb-2 text-center">
+                <p>Teacher</p>
               </div>
               <div className="flex flex-col">
                 <Button text="Login as Teacher"/>
                 </div>
             </div>
             <div className="w-1/2 ml-4">
-              <div className="mb-2">
-                <p>Text 2</p>
+              <div className="mb-2 text-center">
+                <p>Student</p>
               </div>
               <div className="flex flex-col">
                 <Button text="Login as Student"/>
