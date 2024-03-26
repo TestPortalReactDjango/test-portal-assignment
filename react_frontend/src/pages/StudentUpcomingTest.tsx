c28562c05ecdbd45aa123ab025a26f4853c41b6a
dixittanisha02 <dixittanisha02@gmail.com>
2024-03-27 00:03:44 +0530

    feat adds navigation to the student navigation bar feat adds routes for student pages

---
+++ react_frontend/src/pages/StudentUpcomingTest.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavigationBar from "../components/StudentNavigationBar";
+
+const StudentUpcomingTest: React.FC = () => {
+  return (
+    <div>
+      <NavigationBar />
+    </div>
+  );
+};
+
+export default StudentUpcomingTest;
