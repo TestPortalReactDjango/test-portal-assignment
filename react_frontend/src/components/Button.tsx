131279f83088ae690be33b0290a8fa1e6ae07c08
dixittanisha02 <dixittanisha02@gmail.com>
2024-03-28 22:29:02 +0530

    feat adds onClick feature in Button

--- react_frontend/src/components/Button.tsx
+++ react_frontend/src/components/Button.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 interface props{
     text: string;
+    onClick:()=>void;
 }
 const Button: React.FC<props>=(props)=> {
-    const {text}=props;
+    const {text,onClick}=props;
     return (
         <button className="bg-blue-500 text-white rounded py-2 px-4 mb-2">{text}</button>
     )
