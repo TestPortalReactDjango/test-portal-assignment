3f0e790a0890ab9db4eee32e7ad53c5634a54e72
dixittanisha02 <dixittanisha02@gmail.com>
2024-03-28 18:51:39 +0530

    feat modifies GetPastTest api

--- react_frontend/src/api/GetPastTest.tsx
+++ react_frontend/src/api/GetPastTest.tsx
@@ -43,9 +43,18 @@ const GetPastTest = () => {
       loading?(
       <p>Loading...</p>):error?(<p>{error}</p>):(
       <ul>
-        <li></li>
+      {tests.filter(test=> new Date(test.startTime)<=new Date()).map((test) => (
+            <li key={test.pk}>
+              <h6 className="font-bold">{test.testname}</h6>
+              <p>Total Questions: {test.numberOfQuestion.toString()}</p>
+              <p>Start Time: {test.startTime.toString()}</p>
+              <p>End Time: {test.endTime.toString()}</p>
+            </li>
+          ))}
       </ul>
       )
     </div>
   );
 };
+
+export default GetPastTest;
