aaf48c57b131581a7ba13bfca35676eeb20a086d
dixittanisha02 <dixittanisha02@gmail.com>
2024-03-28 22:28:26 +0530

    feat adds select test feature in ongoing test

--- react_frontend/src/api/GetTest.tsx
+++ react_frontend/src/api/GetTest.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Button from "../components/Button";
+import { Link } from "react-router-dom";
 
 interface Test {
   pk: number;
@@ -8,20 +9,22 @@ interface Test {
   numberOfQuestion: Number;
   startTime: Date;
   endTime: Date;
-  //   qRefTable: number;
+  qRefTable: number;
 }
 
 const OngoingTests: Test[] = [];
-
+type nullTest = Test | null;
 const GetTest = () => {
   const [tests, setTests]: [Test[], (tests: Test[]) => void] =
     useState(OngoingTests);
   const [loading, setLoading]: [boolean, (loading: boolean) => void] =
     useState<boolean>(true);
   const [error, setError]: [string, (error: string) => void] = useState("");
+  const [selectedTest, setTest]: [nullTest, (selectedTest: nullTest) => void] =
+    useState<nullTest>(null);
   useEffect(() => {
     axios
-      .get<Test[]>("", {
+      .get<Test[]>("http://127.0.0.1:8000/test/test/", {
         headers: {
           "Content-Type": "application/json",
         },
@@ -41,22 +44,40 @@ const GetTest = () => {
   }, []);
   return (
     <div>
-      loading?(
-      <p>Loading...</p>):error?(<p>{error}</p>):(
-      <ul>
-      {tests.filter(test=> new Date(test.startTime)<=new Date()&&new Date(test.endTime)>=new Date()).map((test) => (
-            <li key={test.pk}>
-              <h6 className="font-bold">{test.testname}</h6>
-              <p>Total Questions: {test.numberOfQuestion.toString()}</p>
-              <p>Start Time: {test.startTime.toString()}</p>
-              <p>End Time: {test.endTime.toString()}</p>
-              <Button text="Start Test"></Button>
-            </li>
-          ))}
-      </ul>
-      )
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : selectedTest ? (
+        <p></p>
+      ) : (
+        <ul>
+          {tests
+            .filter(
+              (test) =>
+                new Date(test.startTime) <= new Date() &&
+                new Date(test.endTime) >= new Date()
+            )
+            .map((test) => (
+              <li key={test.pk}>
+                <h6 className="font-bold">{test.testname}</h6>
+                <p>Total Questions: {test.numberOfQuestion.toString()}</p>
+                <p>Start Time: {test.startTime.toString()}</p>
+                <p>End Time: {test.endTime.toString()}</p>
+                <Link to="">
+                  <Button
+                    text="Start Test"
+                    onClick={() => setTest(test)}
+                  ></Button>
+                </Link>
+              </li>
+            ))}
+        </ul>
+      )}
     </div>
   );
 };
+
+
 
 export default GetTest;
