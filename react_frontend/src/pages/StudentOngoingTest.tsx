7ae569b8dc55857523411ea2a7d2eedf6c57ee46
dixittanisha02 <dixittanisha02@gmail.com>
2024-03-28 22:29:47 +0530

    feat adds feature of listing ongoing tests

--- react_frontend/src/pages/StudentOngoingTest.tsx
+++ react_frontend/src/pages/StudentOngoingTest.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import NavigationBar from "../components/StudentNavigationBar";
-
+import GetTest from "../api/GetTest";
 const StudentOngoingTest: React.FC = () => {
   return (
     <div>
       <NavigationBar />
+      <GetTest/>
     </div>
   );
 };
