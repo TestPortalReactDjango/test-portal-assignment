e33a91fb2f05069a82bc7f513f59a4588bd2cb4b
dixittanisha02 <dixittanisha02@gmail.com>
2024-03-29 20:07:59 +0530

    feat adds routes to TeacherCreateTest and TeacherCreateQuestion pages

--- react_frontend/src/App.tsx
+++ react_frontend/src/App.tsx
@@ -2,36 +2,47 @@ import React from "react";
 import "./App.css";
 // import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import PrivateRoute from '../utils/PrivateRoute';
+import PrivateRoute from "../utils/PrivateRoute";
 import { AuthProvider } from "./context/AuthContext";
 import Home from "./pages/Home";
 import StudentUpcomingTest from "./pages/StudentUpcomingTest";
 import StudentOngoingTest from "./pages/StudentOngoingTest";
 import StudentPastAttempts from "./pages/StudentPastAttempts";
 import StudentProfile from "./pages/StudentProfile";
-import Teacher from "./pages/Teacher";
+import TeacherCreateTest from "./pages/TeacherCreateTest";
+import TeacherCreateQuestion from "./pages/TeacherCreateQuestion";
 import LoginPage from "./pages/Loginpage";
 import SignUpPage from "./pages/signuppage";
 
-const App:React.FC = () => {
+const App: React.FC = () => {
   return (
     <BrowserRouter>
-    <AuthProvider>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/login" element={<Login />} /> */}
-        <Route path="/login_page" element={<LoginPage/>}/>
-        {/* <Route path="/signup" element={<Signup />} /> */}
-        {/* <PrivateRoute> */}
-          <Route path="/studentUpcomingTest" element={<StudentUpcomingTest />} />
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/login" element={<Login />} /> */}
+          <Route path="/login_page" element={<LoginPage />} />
+          {/* <Route path="/signup" element={<Signup />} /> */}
+          {/* <PrivateRoute> */}
+          <Route
+            path="/studentUpcomingTest"
+            element={<StudentUpcomingTest />}
+          />
           <Route path="/studentOngoingTest" element={<StudentOngoingTest />} />
-          <Route path="/studentPastAttempts" element={<StudentPastAttempts />} />
+          <Route
+            path="/studentPastAttempts"
+            element={<StudentPastAttempts />}
+          />
           <Route path="/studentProfile" element={<StudentProfile />} />
-        {/* </PrivateRoute> */}
-        <Route path="/teacher" element={<Teacher />} />
-        <Route path="/signup_page" element={<SignUpPage/>}/>
-      </Routes>
-    </AuthProvider>
+          {/* </PrivateRoute> */}
+          <Route
+            path="/teacherCreateQuestion"
+            element={<TeacherCreateQuestion />}
+          />
+          <Route path="/teacherCreateTest" element={<TeacherCreateTest />} />
+          <Route path="/signup_page" element={<SignUpPage />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 };
