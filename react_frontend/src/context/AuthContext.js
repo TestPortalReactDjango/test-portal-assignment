4a263c2082ead9137091a714049cc8aff3bf73c8
DeenankSharma <sharmadeenank@gmail.com>
2024-03-29 17:51:26 +0530

    bfix : solves login error

--- react_frontend/src/context/AuthContext.js
+++ react_frontend/src/context/AuthContext.js
@@ -27,7 +27,7 @@ export const AuthProvider = ({ children }) => {
     const navigate = useNavigate(); // Use useNavigate instead of useHistory
 
     const loginUser = async (email, password) => {
-        const response = await fetch("http://127.0.0.1:8000/api/token/", {
+        const response = await fetch("http://127.0.0.1:8000/signup/token/", {
             method: "POST",
             headers:{
                 "Content-Type":"application/json"
@@ -71,7 +71,7 @@ export const AuthProvider = ({ children }) => {
     }
 
     const registerUser = async (email, username, password, password2) => {
-        const response = await fetch("http://127.0.0.1:8000/api/register/", {
+        const response = await fetch("http://127.0.0.1:8000/signup/register/", {
             method: "POST",
             headers: {
                 "Content-Type":"application/json"
