720c21ce20be163ac6a5cebd6aa007033972b6a9
dixittanisha02 <dixittanisha02@gmail.com>
2024-03-30 00:23:57 +0530

    feat adds form in TeacherCreateQuestion for selecting type of Question Creation

--- react_frontend/src/pages/TeacherCreateQuestion.tsx
+++ react_frontend/src/pages/TeacherCreateQuestion.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import TeacherNavigationBar from '../components/TeacherNavigationBar';
+import React, { useState } from "react";
+import TeacherNavigationBar from "../components/TeacherNavigationBar";
+import Form from "../components/Form";
 
-const TeacherCreateQuestion:React.FC=()=>{
-    return(
-        <div>
-            <TeacherNavigationBar/>
-        </div>
-    )
-}
+const TeacherCreateQuestion: React.FC = () => {
+  const [Type, setType]: [string, (Type: string) => void] = useState("");
+  return (
+    <div>
+      <TeacherNavigationBar />
+      <div className="flex justify-center items-center h-screen bg-blue-200">
+        {Type === ""}&&
+        <Form
+          choices={[
+            "Single Option Correct Question Type",
+            "Multiple Option Correct Question Type",
+            "Integer Answer Question Type",
+          ]}
+          label={"Choose a Question Type:"}
+          onSubmit={(choice: string) => {
+            setType(choice);
+          }}
+        />
+        {Type === "Single Option Correct Question Type"}&&
+        {Type === "Single Option Correct Question Type"}&&
+        {Type === "Single Option Correct Question Type"}&&
+      </div>
+    </div>
+  );
+};
 
 export default TeacherCreateQuestion;
