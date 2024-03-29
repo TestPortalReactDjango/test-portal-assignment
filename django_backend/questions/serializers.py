949b5a33d0e405ff26f3c5347e9bb6a405ad29b0
dixittanisha02 <dixittanisha02@gmail.com>
2024-03-29 15:23:34 +0530

    feat adds QTypeSerializer

--- django_backend/questions/serializers.py
+++ django_backend/questions/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import SingleCorrectQ,MultipleCorrectQ,IntegerTypeQ,qRefTable
+from .models import SingleCorrectQ,MultipleCorrectQ,IntegerTypeQ,qRefTable,qType
 
 class SCQSerializer(serializers.ModelSerializer):
     class Meta:
@@ -20,3 +20,8 @@ class QRefTableSerializer(serializers.ModelSerializer):
     class Meta:
         model=qRefTable
         fields=['q1','q2','q3','q4','q5','q6','q7','q8','q9','q10','q11','q12','q13','q14','q15','q16','q17','q18','q19','q20','q21','q22','q23','q24','q25','q26','q27','q28','q29','q30']
+
+class QTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=qType
+        fields=['single','multiple','integer']
