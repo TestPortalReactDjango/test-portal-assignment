07fc9d0ddc777b0f94cd56266509a6d7930221b1
dixittanisha02 <dixittanisha02@gmail.com>
2024-03-29 20:06:44 +0530

    feat creates TeacherCreateQuestion page

---
+++ react_frontend/src/pages/TeacherCreateQuestion.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TeacherNavigationBar from '../components/TeacherNavigationBar';
+
+const TeacherCreateQuestion:React.FC=()=>{
+    return(
+        <div>
+            <TeacherNavigationBar/>
+        </div>
+    )
+}
+
+export default TeacherCreateQuestion;
